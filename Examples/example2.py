186bb9785d57487ce7285b99910500f50b4a6a41
Cristel Chandre <cchandre@gmail.com>
2025-07-01 11:55:54 -0400

    Update example2.py

--- Examples/example2.py
+++ Examples/example2.py
@@ -1,20 +1,21 @@
 from hamlorenz import HamLorenz
 
 N = 6
-tf = 6e4
 
 hl = HamLorenz(N)
 
-E = 8.074451109489349
+energy = 8.074451109489349
+casimirs = [2, 18]
 
 a1 = 2.242245751187437
 
-ntraj = 900
+tf = 6e4
+ntraj = 100
 
-x0 = hl.generate_initial_conditions(energy=E, casimirs=[2, 18], ntraj=ntraj)
+x0 = hl.generate_initial_conditions(energy=energy, casimirs=casimirs, ntraj=ntraj)
 
 result = hl.compute_ps(x0, tf, ps=lambda y: y[5] - a1, dir=-1, tol=1e-8)
 
-#hl.save2matlab(result, filename='testdata')
+hl.save2matlab(result, filename='testdata')
 
 hl.plot_ps(result, indices=(0, 1, 2))
